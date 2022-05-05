5e8cca364f11edd80deef1533e521571b405dbd7
lvidal1 <lvida910@gmail.com>
2022-05-05 18:20:42 -0500

    [Feature/sorting] Add filterPositiveAndSortList fx

--- index.js
+++ index.js
@@ -4,3 +4,11 @@
 // 1. Sort input list
 // 2. Create new list
 // 3. Print new list
+
+const filterPositiveAndSortList = list => {
+    return list;
+}
+
+module.exports = {
+    filterPositiveAndSortList
+};
