45ee3d1fec52d062211e2ee10a5106e9db900511
Leonardo Vidal <lvidal910@gmail.com>
2022-05-05 22:21:00 -0500

    Feature/build list (#2)
    * [Feature/build-list] Add buildList fx & failing test case
    
    * [Feature/build list] Implement building list fx
    
    * [Feature/build-list] Add generateRenderList fx & failing test case
    
    * [Feature/build-list] Implement generateRenderList
    
    Co-authored-by: lvidal1 <lvida910@gmail.com>

--- index.js
+++ index.js
@@ -30,9 +30,37 @@ const filterZeros = list => {
     return list.filter(num => num == 0);
 }
 
+/**
+ * Given a list of positive numbers, a list of negative numbers, and a list of zeros, return a list of
+ * the positive numbers, followed by the zeros, followed by the negative numbers.
+ * @param positivePartial array
+ * @param negativePartial array
+ * @param zerosPartial array
+ * @returns array
+ */
+const buildList = (positivePartial, negativePartial, zerosPartial) => {
+    return [...positivePartial, ...zerosPartial, ...negativePartial];
+}
+
+/**
+ * It takes a list of numbers, apply sort & filter mechanism to create an new ordered list 
+ * ready for rendering
+ * @param list array
+ * @returns array.
+ */
+const generateRenderList = (list) => {
+    return buildList(
+        filterPositiveAndSortList(list),
+        filterNegativeAndSortList(list),
+        filterZeros(list),
+    );
+}
+
 
 module.exports = {
     filterPositiveAndSortList,
     filterNegativeAndSortList,
-    filterZeros
+    filterZeros,
+    buildList,
+    generateRenderList
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { filterPositiveAndSortList, filterNegativeAndSortList, filterZeros } = require("./index.js");
+const { filterPositiveAndSortList, filterNegativeAndSortList, filterZeros, buildList, generateRenderList } = require("./index.js");
 
 describe('sort phase', () => {
     test('should filter & sort positive integers', () => {
@@ -15,5 +15,20 @@ describe('sort phase', () => {
         const list = [-1, 0, 3, 0, -4, 0, -3];
         const expected = [0, 0, 0];
         expect(filterZeros(list)).toEqual(expected);
+    });
+})
+
+describe('build phase', () => {
+    test('should receive partials and create new ordered list', () => {
+        const positives = [10, 3, 2];
+        const negatives = [-1, -4];
+        const zeros = [0, 0];
+        const expected = [10, 3, 2, 0, 0, -1, -4];
+        expect(buildList(positives, negatives, zeros)).toEqual(expected);
+    });
+    test('should generate a ordered list for rendering', () => {
+        const list = [-1, 10, 3, 0, -4, 2, 0];
+        const expected = [2, 3, 10, 0, 0, -4, -1];
+        expect(generateRenderList(list)).toEqual(expected);
     });
 })
